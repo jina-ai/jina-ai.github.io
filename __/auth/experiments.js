103a2cfdaf1915f07279cd898d72e69a1892ccad
mapleeit <mapleeit@users.noreply.github.com>
2023-07-27 08:44:06 +0000

    Deploying to master from @ jina-ai/website@7aeb2b587a8d67f51529ea4fa51118ddd25cb203 🚀

---
+++ __/auth/experiments.js
@@ -0,0 +1,7 @@
+(function(){/*
+
+ Copyright The Closure Library Authors.
+ SPDX-License-Identifier: Apache-2.0
+*/
+window.EXPERIMENTS={DISPLAY_CONTINUE_BUTTON_IF_NOT_REDIRECT:{id:1000003,rollout:1,defaultValue:!0,expiration:new Date("April 1, 2020"),stagingRollout:1},CHECK_CONTINUE_URL_IS_AUTHORIZED:{id:1000004,rollout:1,defaultValue:!0,expiration:new Date("September 1, 2020"),stagingRollout:1},POPUP_POST_MESSAGE_TO_IFRAME:{id:1000005,rollout:1,defaultValue:!0,expiration:new Date("October 1, 2020"),stagingRollout:1},CHECK_OAUTH_STATE_STORED_BEFORE_REDIRECT:{id:1000006,rollout:1,defaultValue:!0,expiration:new Date("April 1, 2021"),
+stagingRollout:1}};}).call(this);
---
+++ assets/QSpace.3d360e3b.js
@@ -0,0 +1,1 @@
+import{k as a,m as e}from"./index.7473fdc3.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
