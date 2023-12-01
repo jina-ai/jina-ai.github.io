6d8ff9fe2cb7740f4f04e46503bf5e2833be300c
mapleeit <mapleeit@users.noreply.github.com>
2023-12-01 02:41:45 +0000

    Deploying to master from @ jina-ai/website@3d1deb2d81c265abcf94f79060871fb8c5806dc5 🚀

---
+++ assets/QSpace.46f01bee.js
@@ -0,0 +1,1 @@
+import{i as a,k as e}from"./index.1b823306.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
---
+++ assets/addressbar-color.9fa24b6d.js
@@ -0,0 +1,1 @@
+import{g as a,A as s}from"./index.1b823306.js";var e=()=>{const r=a("primary");r&&s.set(r)};export{e as default};
