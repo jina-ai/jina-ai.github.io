b34619aab2e4862ae3b841003be8ee558cedacad
SmartCodeDavid <SmartCodeDavid@users.noreply.github.com>
2023-12-08 08:32:43 +0000

    Deploying to master from @ jina-ai/website@a22f1db3199f13b44c4d6e39d4be0ad6a3b3476e 🚀

---
+++ assets/QSpace.bcf623c1.js
@@ -0,0 +1,1 @@
+import{i as a,k as e}from"./index.aa6b9a9c.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
---
+++ assets/addressbar-color.0a4a23ab.js
@@ -0,0 +1,1 @@
+import{g as a,A as s}from"./index.aa6b9a9c.js";var e=()=>{const r=a("primary");r&&s.set(r)};export{e as default};
