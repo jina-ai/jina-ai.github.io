28ec08e10d7f4eea88d493405c45e605d8cb99ca
hanxiao <hanxiao@users.noreply.github.com>
2024-03-11 23:52:44 +0000

    Deploying to master from @ jina-ai/website@72f9b850ae65dd59dd7b5030afc2c950b108b27e 🚀

---
+++ assets/QSpace.efbeff53.js
@@ -0,0 +1,1 @@
+import{i as a,k as e}from"./index.3518d94f.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
--- assets/i18n.7bb26d08.js
+++
@@ -1,1 +0,0 @@
-import{b as t,c as n}from"./index.42166537.js";import c from"./index.0e498c82.js";const a="en-US";var g=t(({app:o,store:l})=>{const e=n({locale:a,fallbackLocale:a,legacy:!1,globalInjection:!1,messages:{"en-US":c}});l.use(({store:s})=>{s.t=e.global.t}),o.use(e)});export{g as default};
