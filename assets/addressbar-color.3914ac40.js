035c3e16a52ec663dccff07addc48219114ed0b1
hanxiao <hanxiao@users.noreply.github.com>
2024-07-05 11:13:36 +0000

    Deploying to master from @ jina-ai/website@97527c3cf8e8413591131320305e5afcb69b7d6c 🚀

---
+++ assets/addressbar-color.3914ac40.js
@@ -0,0 +1,1 @@
+import{g as a,A as s}from"./index.9b49e98e.js";var e=()=>{const r=a("primary");r&&s.set(r)};export{e as default};
--- assets/addressbar-color.6ca6f83d.js
+++
@@ -1,1 +0,0 @@
-import{g as a,A as s}from"./index.983cc9f8.js";var e=()=>{const r=a("primary");r&&s.set(r)};export{e as default};
