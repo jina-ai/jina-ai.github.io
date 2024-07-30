6031fba43ac3bdacdc15db78c1255fd2aca97d66
hanxiao <hanxiao@users.noreply.github.com>
2024-07-30 20:26:27 +0000

    Deploying to master from @ jina-ai/website@5e47c10ef2e48f6d58aab30b0b37bae45e9b1674 🚀

---
+++ assets/i18n.92baeb2f.js
@@ -0,0 +1,1 @@
+import{b as t,c as n}from"./index.6bd35c68.js";import c from"./index.6b3a1b75.js";const a="en-US";var g=t(({app:o,store:l})=>{const e=n({locale:a,fallbackLocale:a,legacy:!1,globalInjection:!1,messages:{"en-US":c}});l.use(({store:s})=>{s.t=e.global.t}),o.use(e)});export{g as default};
