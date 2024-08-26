2486cec3721891ba143f1ddadd14295fdecbbd12
mapleeit <mapleeit@users.noreply.github.com>
2024-08-26 07:50:21 +0000

    Deploying to master from @ jina-ai/website@c0523406793073f61bceb0c7051fd5f213c84466 🚀

---
+++ assets/i18n.9bcd1b2d.js
@@ -0,0 +1,1 @@
+import{b as t,c as n}from"./index.68e940e7.js";import c from"./index.d91037ae.js";const a="en-US";var g=t(({app:o,store:l})=>{const e=n({locale:a,fallbackLocale:a,legacy:!1,globalInjection:!1,messages:{"en-US":c}});l.use(({store:s})=>{s.t=e.global.t}),o.use(e)});export{g as default};
