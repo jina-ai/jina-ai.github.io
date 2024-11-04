6d24beb39b6a9a0ac7cd0e10b7fbedf02181c6ef
mapleeit <mapleeit@users.noreply.github.com>
2024-11-04 05:57:51 +0000

    Deploying to master from @ jina-ai/website@aed60213d0da3389bdcb9c73e7d8259213cd1e41 🚀

---
+++ assets/addressbar-color-DwCSTB_W.js
@@ -0,0 +1,1 @@
+import{g as s,A as o}from"./index-eRzPE9b1.js";const t=()=>{const r=s("primary");r&&o.set(r)};export{t as default};
---
+++ assets/use-meta-BgK6PfOq.js
@@ -0,0 +1,1 @@
+import{j as i,w as o,co as t,c7 as u,c6 as l,ag as v,cp as c}from"./index-eRzPE9b1.js";function p(a){{const e={active:!0};if(typeof a=="function"){const n=i(a);e.val=n.value,o(n,s=>{e.val=s,e.active===!0&&t()})}else e.val=a;c.push(e),t(),u(()=>{e.active=!0,t()}),l(()=>{e.active=!1,t()}),v(()=>{c.splice(c.indexOf(e),1),t()})}}export{p as u};
