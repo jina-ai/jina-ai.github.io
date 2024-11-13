519f9ee3f0eb60bb4659664b5835dae1d7058e0d
mapleeit <mapleeit@users.noreply.github.com>
2024-11-13 07:56:34 +0000

    Deploying to master from @ jina-ai/website@2358934090b403169ae1fe72256704a10a09ac7d 🚀

--- assets/use-meta-4gJ6kbri.js
+++
@@ -1,1 +0,0 @@
-import{j as i,w as o,cp as t,c1 as u,c0 as l,ah as v,cq as c}from"./index-CIcplqiH.js";function p(a){{const e={active:!0};if(typeof a=="function"){const n=i(a);e.val=n.value,o(n,s=>{e.val=s,e.active===!0&&t()})}else e.val=a;c.push(e),t(),u(()=>{e.active=!0,t()}),l(()=>{e.active=!1,t()}),v(()=>{c.splice(c.indexOf(e),1),t()})}}export{p as u};
