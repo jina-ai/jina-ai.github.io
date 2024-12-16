50e1abb42d893f9bb44d6076a9e31f70d85716c9
hanxiao <hanxiao@users.noreply.github.com>
2024-12-16 16:11:43 +0000

    Deploying to master from @ jina-ai/website@4f41b662bd5ebd2292cafad60c63717a787f2d03 🚀

--- assets/addressbar-color-BAFePuG_.js
+++
@@ -1,1 +0,0 @@
-import{g as s,A as o}from"./index-DK55Z_Jx.js";const t=()=>{const r=s("primary");r&&o.set(r)};export{t as default};
---
+++ assets/quasar-lang-pack-Lpi9zw7K.js
@@ -0,0 +1,1 @@
+import{b as t,Q as o}from"./index-B2Y1PSBZ.js";const l=Object.assign({}),e=t(async()=>{const a="en-US";try{const n=await l[`../../node_modules/quasar/lang/${a}.mjs`]();o.lang.set(n.default)}catch{}});export{e as default};
