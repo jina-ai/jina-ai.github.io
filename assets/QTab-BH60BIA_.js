95eb5aad6ed8573ed4ba24f9d6aff5ec370441a7
shazhou2015 <shazhou2015@users.noreply.github.com>
2025-01-02 11:28:40 +0000

    Deploying to master from @ jina-ai/website@cd3b48fc0b87e5a4f083215976b3a52b9dad9f48 🚀

---
+++ assets/QTab-BH60BIA_.js
@@ -0,0 +1,1 @@
+import{u as o,a as t,b as m}from"./QTabs-CI64WGMD.js";import{s as p}from"./index-CtIMUL80.js";const u=p({name:"QTab",props:o,emits:t,setup(s,{slots:a,emit:e}){const{renderTab:r}=m(s,a,e);return()=>r("div")}});export{u as Q};
