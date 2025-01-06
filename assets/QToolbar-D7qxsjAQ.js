4e961b451dc9417f6a392b4fdc85e8024b38c14f
shazhou2015 <shazhou2015@users.noreply.github.com>
2025-01-06 09:52:45 +0000

    Deploying to master from @ jina-ai/website@275d75c0a5e62fb94bfd52142c2e6c8c53ef0906 🚀

--- assets/QToolbar-D7qxsjAQ.js
+++
@@ -1,1 +0,0 @@
-import{s,m as a,p as r,K as l}from"./index-oQkYlwQx.js";const p=s({name:"QToolbar",props:{inset:Boolean},setup(o,{slots:e}){const t=a(()=>"q-toolbar row no-wrap items-center"+(o.inset===!0?" q-toolbar--inset":""));return()=>r("div",{class:t.value,role:"toolbar"},l(e.default))}});export{p as Q};
---
+++ assets/useRoute-CMr_vn1T.js
@@ -0,0 +1,1 @@
+import{u as e,ak as a}from"./index-C8FEXe77.js";const o=e(),{user:r}=a(o),u=s=>{r.value.uid?s.push({path:"/api-dashboard/key-manager"}):s.push({path:"/api-dashboard",query:{login:"true"}})};export{u as s};
