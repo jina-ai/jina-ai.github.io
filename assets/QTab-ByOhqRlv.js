e81728aec5029a39bade27f73d31015436e3ef5c
hanxiao <hanxiao@users.noreply.github.com>
2025-02-01 01:09:26 +0000

    Deploying to master from @ jina-ai/website@360c6e80e64939702a4a8fdca51b11143bdf711d 🚀

---
+++ assets/QTab-ByOhqRlv.js
@@ -0,0 +1,1 @@
+import{u as o,a as t,b as m}from"./QTabs-_5DAh_zQ.js";import{s as p}from"./index-BpYg73uG.js";const u=p({name:"QTab",props:o,emits:t,setup(s,{slots:a,emit:e}){const{renderTab:r}=m(s,a,e);return()=>r("div")}});export{u as Q};
