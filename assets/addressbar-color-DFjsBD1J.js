3e0eeb6fc922f7f02976c754413f9c33abf53219
hanxiao <hanxiao@users.noreply.github.com>
2025-02-14 16:24:40 +0000

    Deploying to master from @ jina-ai/website@e5b9377ec2b19f2453c3a845b8aa723d1332a72b 🚀

---
+++ assets/addressbar-color-DFjsBD1J.js
@@ -0,0 +1,1 @@
+import{g as s,A as o}from"./index-CZ7uG9Zj.js";const t=()=>{const r=s("primary");r&&o.set(r)};export{t as default};
---
+++ assets/useRoute-DOq0wMcc.js
@@ -0,0 +1,1 @@
+import{u as e,am as a}from"./index-CZ7uG9Zj.js";const o=e(),{user:r}=a(o),u=s=>{r.value.uid?s.push({path:"/api-dashboard/key-manager"}):s.push({path:"/api-dashboard",query:{login:"true"}})};export{u as s};
