15dc4ae0a9b30e34a18ca7ec10984080bd45670e
hanxiao <hanxiao@users.noreply.github.com>
2025-03-03 01:27:40 +0000

    Deploying to master from @ jina-ai/website@05d9b888fa5b0ec1020b49b202c62054b06468be 🚀

--- assets/useRoute-CUnxXj1r.js
+++
@@ -1,1 +0,0 @@
-import{u as e,am as a}from"./index-Ck1z1-K0.js";const o=e(),{user:r}=a(o),u=s=>{r.value.uid?s.push({path:"/api-dashboard/key-manager"}):s.push({path:"/api-dashboard",query:{login:"true"}})};export{u as s};
