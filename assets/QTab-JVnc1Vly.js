f59e8db39c31ebb81249896967bd7ad6d8a6c2cb
hanxiao <hanxiao@users.noreply.github.com>
2025-04-22 06:15:50 +0000

    Deploying to master from @ jina-ai/website@6ad5b977fc3f78ecc53eb9919dcee407d1f3ffc3 🚀

---
+++ assets/QTab-JVnc1Vly.js
@@ -0,0 +1,1 @@
+import{u as o,a as t,b as m}from"./QTabs-mrXqZuyP.js";import{s as p}from"./index-BcKXXr3y.js";const u=p({name:"QTab",props:o,emits:t,setup(s,{slots:a,emit:e}){const{renderTab:r}=m(s,a,e);return()=>r("div")}});export{u as Q};
---
+++ assets/addressbar-color-DT3LEHzB.js
@@ -0,0 +1,1 @@
+import{g as s,A as o}from"./index-BcKXXr3y.js";const t=()=>{const r=s("primary");r&&o.set(r)};export{t as default};
